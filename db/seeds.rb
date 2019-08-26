402700e84d3f5b477b5c14ae42d8f06c0050d727
Pierrick Delage <pierrick.delage@gmail.com>
2019-08-26 18:16:18 +0200

    Seed Users added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+User.destroy_all
+Formation.destroy_all
+Job.destroy_all
+
+puts 'Creating 3 Users...'
+
+jean_michel = User.new(
+  first_name: 'Jean',
+  last_name: 'Michel',
+  age: 23,
+  email: "jean@michel.com",
+  password: "password",
+)
+
+jean_michel.save!
+
+francoise_marie = User.new(
+  first_name: 'Francoise',
+  last_name: 'Marie',
+  age: 25,
+  email: "francoise@marie.com",
+  password: "password"
+
+)
+
+francoise_marie.save!
+
+jacques_martin = User.new(
+  first_name: 'Jacques',
+  last_name: 'Martin',
+  age: 22,
+  email: "jacques@martin.com",
+  password: "password"
+)
+
+jacques_martin.save!
+
+puts 'Your database has been updated successfully!'
