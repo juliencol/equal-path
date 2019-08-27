580b9009cbc734015493c7e99527bcd40a954901
gaetanok <ok.gaetan@gmail.com>
2019-08-27 12:44:29 +0200

    search bar test

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,13 @@
 class JobsController < ApplicationController
-
   def index
-    @jobs = Job.all
+    if params[:query].present?
+      @jobs = Job.where("title ILIKE ?", "%#{params[:query]}%")
+    else
+      @jobs = Job.all
+    end
+  end
 
   def show
     @job = Job.find(params[:id])
-
   end
 end
