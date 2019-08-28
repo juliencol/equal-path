72c83253d84b28d9baaa9b976b85e7ee589bbbba
gaetanok <ok.gaetan@gmail.com>
2019-08-28 18:23:28 +0200

    search bar de merde

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
 import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+import { initSelect2 } from '../plugins/init_select2';
+initSelect2();
