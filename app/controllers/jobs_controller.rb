2dd55aedc1b27ef697f0f532e4b21582fa47d56b
Julien Colombain <julien@colombain.com>
2019-08-28 13:16:58 +0200

    bookmark jobs feature added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,4 +12,10 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def bookmark
+    job = Job.find(params[:job_id])
+    job.bookmarked ? job.update(bookmarked: false) : job.update(bookmarked: true)
+    redirect_to jobs_path
+  end
 end
