5fd457049c276dd3be31197b044b9e6932dc9842
gaetanok <ok.gaetan@gmail.com>
2019-08-29 12:18:09 +0200

    search bar avancé

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'uglifier'
 gem 'webpacker'
 gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
-
+gem 'pg_search', '~> 2.3.0'
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,11 +1,15 @@
 class JobsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
+    @jobs = Job.all
     if params[:query].present?
-      @jobs = Job.where("title ILIKE ?", "%#{params[:query]}%")
-
-    else
-      @jobs = Job.all
+      @jobs = @jobs.global_search(params[:query])
+    end
+    if params[:skill].present?
+      @jobs = @jobs.global_search(params[:skill])
+    end
+     if params[:field].present?
+      @jobs = @jobs.global_search(params[:field])
     end
   end
 
