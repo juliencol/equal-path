182d9c7b2ce73d0e89de46ef10c73925b4ca8934
gaetanok <ok.gaetan@gmail.com>
2019-08-29 12:16:16 +0200

    search bar avancé

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
 import 'select2';
+import '../../../node_modules/select2/dist/css/select2.css';
+
 
 const initSelect2 = () => {
-  $('.select2').select2(); // (~ document.querySelectorAll)
+  $('.select2').select2();
 };
 
 export { initSelect2 };
