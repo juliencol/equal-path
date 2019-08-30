0fe9b6dd6960a1b823f9a9c07b83b30cf40061e5
Pierrick Delage <pierrick.delage@gmail.com>
2019-08-30 11:51:46 +0200

    Profile completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
-
   def index
     @jobs = Job.where(bookmarked: true)
     @formations = Formation.where(bookmarked: true)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
