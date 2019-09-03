074b55da6f187e22bb3b88c98e767ab70ab8b5ef
Julien Colombain <julien@colombain.com>
2019-09-03 15:49:12 +0200

    Now the JS dashboard works and I know why and how

--- app/javascript/components/dashboard.js
+++ app/javascript/components/dashboard.js
@@ -1,26 +1,26 @@
-const dashboard_animation = () => {
-  const jobsBtn = document.getElementById('jobs-btn');
-  const formationsBtn = document.getElementById('formations-btn');
-  const favJobs = document.getElementById('fav-jobs');
-  const favFormations = document.getElementById('fav-formations');
-
-  if (jobsBtn) {
-    jobsBtn.addEventListener("click", (event) => {
-      formationsBtn.classList.remove("active");
-      jobsBtn.classList.add("active");
-      favJobs.classList.remove("d-none");
-      favFormations.classList.add("d-none");
+const dashboardTabs = () => {
+  const dashboard_tabs = document.querySelectorAll(".tab-underlined");
+  // pour chaque tab link
+  dashboard_tabs.forEach((dashboard_tab) => {
+    // écouter le click
+    dashboard_tab.addEventListener("click", (event) => {
+      // Do something (callback)
+      // selectionner tous les tab-content
+      const hide_tabs = document.querySelectorAll(".tab-content");
+      // leur mettre à tous la classe d-none
+      hide_tabs.forEach((hide_tab) => {
+        hide_tab.classList.add("d-none");
+      })
+      const active_tab = document.querySelector(".tab-underlined.active");
+      console.log(active_tab);
+      active_tab.classList.remove("active");
+      event.currentTarget.classList.add("active");
+      // recupere le data-target du lien cliqué
+      const shown_tab = document.getElementById(event.currentTarget.dataset.target);
+      // selectionner le tab-content à affiher => enlever la classe d-none
+      shown_tab.classList.remove("d-none");
     });
-
-    formationsBtn.addEventListener("click", (event) => {
-      jobsBtn.classList.remove("active");
-      formationsBtn.classList.add("active");
-      favFormations.classList.remove("d-none");
-      favJobs.classList.add("d-none");
-    });
-  }
-
+  })
 };
 
-export { dashboard_animation };
-
+export { dashboardTabs };
--- app/javascript/components/job_show_tabs.js
+++ app/javascript/components/job_show_tabs.js
@@ -24,6 +24,3 @@ const jobShowTabs = () => {
 };
 
 export { jobShowTabs };
-
-
-
