ea8a82aca8fea76b42ff2e01bf972ccc78f834d7
Julien Colombain <julien@colombain.com>
2019-09-03 15:00:26 +0200

    I don't know how and why but the dashboard tabs js appears to be working fine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
     @jobs = Job.where(bookmarked: true)
     @formations = Formation.where(bookmarked: true)
   end
