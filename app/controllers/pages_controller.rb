c2e0c896dfec87ba3b758b50eabc51706118bfd1
Pierrick Delage <pierrick.delage@gmail.com>
2019-09-04 10:21:50 +0200

    c'est parti fonctionne

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@ class PagesController < ApplicationController
 
   def home
     @user = current_user
+    if @user.nil?
+      @path_one = new_user_session_path
+    else
+      @path_one = edit_user_path(@user)
+    end
   end
 end
