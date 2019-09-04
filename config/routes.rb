8fd523f4daaa38cf6102ede8e3c5dbde7a27df4c
Julien Colombain <julien@colombain.com>
2019-09-04 13:14:31 +0200

    dashboard style updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   resources :jobs, only: [:show, :index] do
     post "bookmark_job", to: 'jobs#bookmark_job'
     post "bookmark_formation", to: 'jobs#bookmark_formation'
-    post "remove_from_fav", to: 'jobs#remove_from_fav'
+    post "remove_job_from_fav", to: 'jobs#remove_job_from_fav'
+    post "remove_formation_from_fav", to: 'formations#remove_formation_from_fav'
   end
 
   resources :users, only: [:index, :show, :edit]
   resources :skills, only: [] do
     post "update_skill", to: 'users#update_skill'
   end
+
   resources :formations, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
