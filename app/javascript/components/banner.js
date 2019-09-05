bf3730e87e37dc092659c466eecad5b66365fb21
gaetanok <ok.gaetan@gmail.com>
2019-09-05 16:12:08 +0200

    index degeu

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: [" Decouvre de nouvelles opportunités.", "Trouve ton orientation de carrière"],
-    typeSpeed: 70,
-    loop: true
-  });
+  if (document.getElementById("banner-typed-text")) {
+    new Typed('#banner-typed-text', {
+      strings: [" Decouvre de nouvelles opportunités.", "Trouve ton orientation de carrière"],
+      typeSpeed: 70,
+      loop: true
+    });
+
+  }
 }
 
 export { loadDynamicBannerText };
